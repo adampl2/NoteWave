de8a543fabd4b07618ee5ff179dad497da81458e
adampl2 <adamchwiulkowski@gmail.com>
2023-03-27 06:31:50 +0000

    import User model

--- sweatsmart/__init__.py
+++ sweatsmart/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     app.register_blueprint(routes, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User, Trip
 
     with app.app_context():
         db.create_all()
