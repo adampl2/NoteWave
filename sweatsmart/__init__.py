b540fe2f71f476df67ab478882eaf3d96b101b01
adampl2 <adamchwiulkowski@gmail.com>
2023-03-28 00:32:26 +0000

    create app & create database

--- sweatsmart/__init__.py
+++ sweatsmart/__init__.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 from flask_login import LoginManager
 if os.path.exists("env.py"):
     import env  # noqa
-
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,7 +12,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'sweatsmart_key'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -22,8 +22,8 @@ def create_app():
     app.register_blueprint(routes, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Trip
-
+    from .models import User, Note
+    
     with app.app_context():
         db.create_all()
 
