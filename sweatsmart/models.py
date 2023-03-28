cdee9bd7935a863c87fe132a818c2321af90f327
adampl2 <adamchwiulkowski@gmail.com>
2023-03-28 00:33:09 +0000

    Note model created

--- sweatsmart/models.py
+++ sweatsmart/models.py
@@ -3,24 +3,16 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Trip(db.Model):
-    # schema for the Trip model
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    trip_name = db.Column(db.String(50), unique=True, nullable=False)
-    trip_description = db.Column(db.Text, nullable=False)
-    trip_start_date = db.Column(db.Date, nullable=False)
-    trip_end_date = db.Column(db.Date, nullable=False)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        # __repr__ to represent itself in the form of a string
-        return
-        f"#{self.id} - Task: {self.trip_name}"
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    username = db.Column(db.String(120))
-    trip = db.relationship('Trip')
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(150))
+    notes = db.relationship('Note')
