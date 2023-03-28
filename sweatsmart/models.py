11004cbc20fb6611d3d2e8cf4ab11ce76d0b6a00
adampl2 <adamchwiulkowski@gmail.com>
2023-03-28 02:45:01 +0000

    title variable included in Note model

--- sweatsmart/models.py
+++ sweatsmart/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.sql import func
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
+    title = db.Column(db.String(30))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
