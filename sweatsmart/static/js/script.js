b09f8940dfd3885c8e61103e6db1ecde38518e9c
adampl2 <adamchwiulkowski@gmail.com>
2023-03-28 00:36:06 +0000

    Delete note function

--- sweatsmart/static/js/script.js
+++ sweatsmart/static/js/script.js
@@ -1,25 +1,8 @@
-/**
- * Shrinks nav bar, removes padding and adds a black background.
- */
-
-$(window).scroll(function () {
-    if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
-        console.log("OK");
-    } else {
-        $('.nav').removeClass('affix');
-    }
-});
-
-/**
- * This function toggles the "active" class on the navTrigger 
- * element and shows or hides the mainListDiv element when clicked
- */
-
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
-});
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
