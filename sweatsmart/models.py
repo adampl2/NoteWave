47f58dcc8f35e6a0f58b0bf6822fb404f4d8a190
adampl2 <adamchwiulkowski@gmail.com>
2023-04-01 04:27:36 +0000

    models.py: def __repr__ added

--- sweatsmart/models.py
+++ sweatsmart/models.py
@@ -10,10 +10,17 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    def __repr__(self):
+        return 'ID %r - Note: 0x%x' % (
+            self.id, self.title)
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150))
-    username = db.Column(db.String(150))
+    username = db.Column(db.String(80), unique=True, nullable=False)
     notes = db.relationship('Note')
+
+    def __repr__(self):
+        return '| User %r |' % self.username
