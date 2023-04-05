e6a19d2a3e960c414c77f2db7f974f38374da31e
adampl2 <adamchwiulkowski@gmail.com>
2023-04-05 19:52:31 +0000

    Modal function

--- sweatsmart/static/js/script.js
+++ sweatsmart/static/js/script.js
@@ -1,10 +1,10 @@
-function deleteNote(noteId) {
-  const confirmed = confirm("Are you sure you want to delete this note?");
-  if (!confirmed) return;
-  fetch("/delete-note?confirm=yes", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
+function showConfirmDeleteModal(noteId) {
+  let deleteUrl = "/delete-note/" + noteId;
+  $('#delete-confirm-button').attr('href', deleteUrl);
+  $('#confirmDeleteModal').modal('show');
 }
+
+
+
+
+
