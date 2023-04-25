e2a077fef2adfb2c517c67dcc144198f85ed8369
adampl2 <adamchwiulkowski@gmail.com>
2023-04-25 00:56:46 +0000

    DOM content loaded listener added

--- notewave/static/js/script.js
+++ notewave/static/js/script.js
@@ -1,16 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+  /** Hides flash message after 5 seconds of inactivity */
+
+  setTimeout(function () {
+    $('#flashMessage').fadeOut(1500, "linear");
+  }, 5000);
+
+  /** Gets the current year to display it in the footer element */
+
+  document.getElementById("year").innerHTML = new Date().getFullYear();
+});
+
+/** Shows the modal and deletes the note from the database  */
+
 function showConfirmDeleteModal(noteId) {
   let deleteUrl = "/delete-note/" + noteId;
   $('#delete-confirm-button').attr('href', deleteUrl);
   $('#confirmDeleteModal').modal('show');
 }
 
-/** Hides flash message after 5 seconds of inactivity */
+addEventListener('DOMContentLoaded', showConfirmDeleteModal);
 
-setTimeout(function() {
-  $('#flashMessage').fadeOut(1500, "linear");
-}, 5000);
 
-document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
 
