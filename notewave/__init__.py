700ded836d031e30725d8860a6705d08be625c33
adampl2 <adamchwiulkowski@gmail.com>
2023-04-27 23:51:12 +0000

    Heroku reqs

--- notewave/__init__.py
+++ notewave/__init__.py
@@ -9,6 +9,16 @@ if os.path.exists("env.py"):
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
 
 def create_app():
     app = Flask(__name__)
